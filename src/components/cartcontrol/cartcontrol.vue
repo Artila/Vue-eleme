3065a941a9c02262cf98192057121b993bbb4633
Artila <529276960@qq.com>
2017-06-23 09:41:19 +0800

    购物车

---
+++ src/components/cartcontrol/cartcontrol.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="cartcontrol">
+    <transition name="move">
+      <div class="cart-decrease" v-show="food.count>0" @click.stop.prevent="decreaseCart">
+        <span class="inner icon-remove_circle_outline"></span>
+      </div>
+    </transition>
+    <!--<div class="cart-decrease icon-remove_circle_outline" v-show="food.count>0" @click="decreaseCart"></div>-->
+    <div class="cart-count" v-show="food.count>0">{{food.count}}</div>
+    <div class="cart-add icon-add_circle" @click.stop.prevent="addCart"></div>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  import Vue from 'vue';
+
+  export default {
+    props: {
+      food: {
+        type: Object
+      }
+    },
+    methods: {
+      addCart(event) {
+        if (!event._constructed) {
+          return;
+        }
+        if (!this.food.count) {
+          Vue.set(this.food, 'count', 1);
+        } else {
+          this.food.count++;
+        }
+        // this.$dispatch('cart.add', event.target); vue 2.0 不能用dispatch
+        this.$emit('add', event.target);
+      },
+      decreaseCart(event) {
+        if (!event._constructed) {
+          return;
+        }
+        if (this.food.count) {
+          this.food.count--;
+        }
+      }
+    }
+  };
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+  @import "./cartcontrol.styl";
+</style>
